3b8dfee8cba21cd0435dc9c7d4dca0c839e1b06f
Luan <luan.emanuelriar@gmail.com>
2025-05-25 11:48:19 -0300

    Doc - criação do arquivo README

---
+++ README.md
@@ -0,0 +1,41 @@
+# Bot para discord
+
+### Instalação
+
+1. Clone o repositório
+```bash
+git clone https://github.com/Vinicius-Mendes/bot-discord.git
+```
+
+2. Crie o ambiente virutal python
+```bash
+python -m venv .venv
+```
+
+3. Ative o ambiente virutal
+    a. Linux
+    ```bash
+    source .venv/bin/activate
+    ```
+    b. Windows
+    ```bash
+    .venv\Scripts\activate
+    ```
+
+4. Instale as dependências
+```bash
+pip install -r requirements.txt
+```
+
+5. Crie o arquivo .env com as variáveis de ambiente (exemplo de como deve ficar o arquivo em .env.example)
+    - Chaves de API obtidas nos sites oficiais do Discord DEV e Google Gemini API
+
+6. Inicie a aplicação com:
+```bash
+python main.py
+```
+
+### Extras:
+- Documentação da API do Discord: https://discord.com/developers/docs/intro
+- Documentação da API do Google Gemini: https://developers.generativeai.google/
+- Documentação da biblioteca discord.py : https://discordpy.readthedocs.io/en/stable/index.html
--- main.py
+++ main.py
@@ -12,39 +12,41 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     print("bot is running...")
-        
+
+
 @bot.command()
 async def teste(ctx, arg: Optional[str] = ''):
     await ctx.send(f"Hello {arg}!")
+
 
 @bot.command()
 async def roll(ctx, dice: Optional[str]):
     if not dice:
         await ctx.send("Informe sua rolada")
         return
-    
-    
-    try: 
+
+    try:
         numero_de_dados, numero_de_faces = map(int, dice.split('d'))
     except Exception:
         await ctx.send("Formato invalido inserido, não é um numero. Roll deve ester no formato NdN ! ")
         return
-    
-    
+
     if numero_de_dados < 1 or numero_de_dados > 10000:
         await ctx.send("Número de dados inválido (range valido: 1 -> 10000)")
         return
-    
+
     if numero_de_faces < 2:
         await ctx.send("Número de faces inválido")
         return
-    
+
     results = []
-    results = [random.randint(1, numero_de_faces) for _ in range(numero_de_dados)]
+    results = [random.randint(1, numero_de_faces)
+               for _ in range(numero_de_dados)]
     results_formatado = ', '.join(map(str, results))
     message = f"""
     :game_die: Rolada de {dice}
@@ -53,11 +55,11 @@ async def roll(ctx, dice: Optional[str]):
     """
     await ctx.send(message)
 
+
 @bot.command()
 async def ai(ctx, *, prompt):
-    response = ai_client.models.generate_content(model="gemini-1.5-flash", contents=prompt)
+    response = ai_client.models.generate_content(
+        model="gemini-1.5-flash", contents=prompt)
     await ctx.send(response.text)
-        
-bot.run(os.getenv("BOT_TOKEN"))
 
-    
+bot.run(os.getenv("BOT_TOKEN"))
